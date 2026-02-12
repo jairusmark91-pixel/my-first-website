cdd6bdc62a3dc6c4c19f07bf35c64c0ced8f7ca8
jairusmark91-pixel <jairusmark91@gmail.com>
2026-02-12 21:14:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function sayHello() {
-    document.querySelector("p").textContent =
-        "hello";
-}
+onload = () => {
+    document.body.classList.remove("container");
+  };
+ 
+  const wrapper = document.querySelector(".wrapper");
+  const openBtn = document.getElementById("openBtn");
+  const closeBtn = document.getElementById("closeBtn");
+  
+  openBtn.addEventListener("click", () => {
+      wrapper.classList.add("open");
+      openBtn.style.display = "none";
+      closeBtn.style.display = "inline-block";
+  });
+  
+  closeBtn.addEventListener("click", () => {
+      wrapper.classList.remove("open");
+      closeBtn.style.display = "none";
+      openBtn.style.display = "inline-block";
+  });
+  
